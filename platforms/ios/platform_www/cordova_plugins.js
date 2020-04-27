ec5f00d986ecb50f17a0a897ccde4c55c2f65f16
Izam Basiron <izam.shah@gmail.com>
2020-04-27 21:20:42 +0800

    Initial commit

---
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "com.outsystems.documentpreview.DocumentPreview",
+      "file": "plugins/com.outsystems.documentpreview/www/DocumentPreview.js",
+      "pluginId": "com.outsystems.documentpreview",
+      "clobbers": [
+        "window.plugins.DocumentPreview"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.4",
+    "com.outsystems.documentpreview": "1.1.1"
+  };
+});
---
+++ platforms/ios/www/plugins/com.outsystems.documentpreview/www/DocumentPreview.js
@@ -0,0 +1,8 @@
+cordova.define("com.outsystems.documentpreview.DocumentPreview", function(require, exports, module) {
+var exec = require('cordova/exec');
+
+exports.openDocument = function(filePath, fileMIMEType, previewDoc, success, error) {
+    exec(success, error, "DocumentPreview", "openDocument", [filePath, fileMIMEType, previewDoc]);
+};
+
+});
